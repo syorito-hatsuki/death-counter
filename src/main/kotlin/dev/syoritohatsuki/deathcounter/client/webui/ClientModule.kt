d19f231fda0a384ed9cbcbd1bd39f6e79e144693
syorito-hatsuki <enderdron3@gmail.com>
2023-03-04 22:46:46 +0200

    Added toast notification on WebUI started

--- src/main/kotlin/dev/syoritohatsuki/deathcounter/client/webui/ClientModule.kt
+++ src/main/kotlin/dev/syoritohatsuki/deathcounter/client/webui/ClientModule.kt
@@ -1,30 +1,34 @@
 package dev.syoritohatsuki.deathcounter.client.webui
 
-import dev.syoritohatsuki.deathcounter.legacy.client.manager.ClientConfigManager
+import dev.syoritohatsuki.deathcounter.client.ClientConfigManager
+import dev.syoritohatsuki.deathcounter.client.extension.getDeathCount
+import dev.syoritohatsuki.deathcounter.client.toast.WebToast
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
 import io.ktor.server.html.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import net.minecraft.client.MinecraftClient
-import net.minecraft.text.Text
 
 fun Application.clientModule(client: MinecraftClient) {
-    routing {
-        get("deaths/{playerName}") {
-            /*   TODO respond player death count from cache file    */
-        }
-        get("/{playerName}") {
-            call.respondHtml { htmlTemplate(call.parameters["playerName"].toString()) }
-        }
-        get {
-            call.respondHtml { htmlTemplate(client.player?.entityName.toString()) }
-        }
-    }
+    (environment as ApplicationEngineEnvironment).connectors.also { con ->
 
-    /*   TODO Change chat message to Toast notification   */
-    client.player?.let { clientPlayerEntity ->
-        clientPlayerEntity.sendMessage(Text.literal(""))
-        clientPlayerEntity.sendMessage(Text.literal("§a§lWebUI Started"))
-        clientPlayerEntity.sendMessage(Text.literal("§7Access URL: §9§nhttp://0.0.0.0:${ClientConfigManager.read().port}"))
-        clientPlayerEntity.sendMessage(Text.literal(""))
+        environment.monitor.subscribe(ApplicationStarted) {
+            if (!ClientConfigManager.read().showToastNotification.disable) WebToast.show(
+                client.toastManager,
+                con[0].host,
+                con[0].port
+            )
+        }
+
+        routing {
+            get {
+                call.respondHtml { htmlTemplate(con[0].host, con[0].port) }
+            }
+
+            get("deaths") {
+                call.respond(client.player?.getDeathCount().toString())
+            }
+        }
     }
 }
