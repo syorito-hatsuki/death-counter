418bcf0ad5a4c0a65175659aa4b28641abc0eb28
syorito-hatsuki <enderdron3@gmail.com>
2023-03-04 17:05:41 +0200

    Removed legacy package

--- src/main/kotlin/dev/syoritohatsuki/deathcounter/legacy/client/dto/ClientConfig.kt
+++
@@ -1,9 +0,0 @@
-package dev.syoritohatsuki.deathcounter.legacy.client.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ClientConfig(
-    val port: Int = 3000,
-    val msDelay: Int = 5000
-)
