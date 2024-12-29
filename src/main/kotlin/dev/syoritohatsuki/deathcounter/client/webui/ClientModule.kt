44a88149ecc162dc2975dd31410be8c1b07aa17f
syorito-hatsuki <enderdron3@gmail.com>
2024-12-29 04:16:18 +0200

    Downgrade from 1.21.2+

--- src/main/kotlin/dev/syoritohatsuki/deathcounter/client/webui/ClientModule.kt
+++ src/main/kotlin/dev/syoritohatsuki/deathcounter/client/webui/ClientModule.kt
@@ -4,29 +4,43 @@ import dev.syoritohatsuki.deathcounter.client.ClientConfigManager
 import dev.syoritohatsuki.deathcounter.client.extension.getDeathCount
 import dev.syoritohatsuki.deathcounter.client.toast.WebToast
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.html.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.launch
 import net.minecraft.client.MinecraftClient
 
 fun Application.clientModule(client: MinecraftClient) {
-    (environment as ApplicationEngineEnvironment).connectors.also { con ->
 
-        environment.monitor.subscribe(ApplicationStarted) {
-            if (!ClientConfigManager.read().showToastNotification.disable) WebToast.show(
-                client.toastManager, con[0].host, con[0].port
-            )
+    var host = ""
+    var port = 0
+
+    monitor.subscribe(ApplicationStarted) {
+        launch {
+            val connector = engine.resolvedConnectors().firstOrNull()
+            if (connector != null) {
+                host = connector.host
+                port = connector.port
+
+                if (!ClientConfigManager.read().showToastNotification.disable) {
+                    client.toastManager.add(WebToast(host, port))
+                }
+            } else {
+                log.error("No connectors found!")
+            }
+        }
+    }
+
+
+    routing {
+        get {
+            call.respondHtml {
+                htmlTemplate(host, port)
+            }
         }
 
-        routing {
-            get {
-                call.respondHtml { htmlTemplate(con[0].host, con[0].port) }
-            }
-
-            get("deaths") {
-                call.respondText(client.player?.getDeathCount().toString())
-            }
+        get("deaths") {
+            call.respondText(client.player?.getDeathCount().toString())
         }
     }
 }
--- src/main/kotlin/dev/syoritohatsuki/deathcounter/util/specialdays/SpecialDays.kt
+++ src/main/kotlin/dev/syoritohatsuki/deathcounter/util/specialdays/SpecialDays.kt
@@ -3,5 +3,19 @@ package dev.syoritohatsuki.deathcounter.util.specialdays
 import java.time.LocalDate
 
 enum class SpecialDays(val from: LocalDate, val to: LocalDate, val path: String) {
-    HALLOWEEN(LocalDate.of(LocalDate.now().year, 10, 31), LocalDate.of(LocalDate.now().year, 11, 1), "events/halloween")
+    HALLOWEEN(
+        LocalDate.of(LocalDate.now().year, 10, 31),
+        LocalDate.of(LocalDate.now().year, 11, 1),
+        "events/halloween"
+    ),
+    CHRISTMAS(
+        LocalDate.of(LocalDate.now().year, 12, 24),
+        LocalDate.of(LocalDate.now().year, 12, 25),
+        "events/christmas"
+    ),
+    NEW_YEAR(
+        LocalDate.of(LocalDate.now().year, 1, 1),
+        LocalDate.of(LocalDate.now().year, 1, 1),
+        "events/new_year"
+    )
 }
